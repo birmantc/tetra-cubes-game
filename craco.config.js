cd6d6f93b3cec79b4d621008091355bde1a349ff
birmantc <birman.tc@gmail.com>
2022-03-08 00:43:30 +0300

    chore: ignore build config from linting

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      // eslint-disable-next-line no-param-reassign
-      webpackConfig.resolve.fallback = {
-        fs: false,
-        stream: false,
-        path: require.resolve("path-browserify"),
-        crypto: require.resolve("crypto-browserify")
-      };
-      return webpackConfig;
-    },
-  },
+	webpack: {
+		configure: (webpackConfig, { env, paths }) => {
+			// eslint-disable-next-line no-param-reassign
+			webpackConfig.resolve.fallback = {
+				fs: false,
+				stream: false,
+				path: require.resolve('path-browserify'),
+				crypto: require.resolve('crypto-browserify'),
+			};
+			return webpackConfig;
+		},
+	},
 };
